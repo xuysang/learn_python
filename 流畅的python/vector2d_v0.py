6e026ee099ac48a8202d235fd5862d854c175a38
xuysang <807692755@qq.com>
2019-10-18 20:41:14 +0800

    2

--- 流畅的python/vector2d_v0.py
+++ 流畅的python/vector2d_v0.py
@@ -9,17 +9,17 @@ class Vector2d:
 		return (i for i in (self.x,self.y))  # 直接调用生成器表达式一个接一个产出分量
 	def __repr__(self):
 		class_name = type(self).__name__
-		return '{}({!r},{!r})'.format(class_name,*self)
+		return '{}({!r},{!r})'.format(class_name,*self) #使用{!r}获取各个分量的表示形式，然后插值，构成一个字符串。因为Vector2d实例是可迭代对象，所以*self会把x和y分量提供给format函数。
 	def __str__(self):
-		return str(tuple(self))
+		return str(tuple(self))  # 从可迭代的Vector2d实例中得到一个元祖，显示为一个有序对。
 	def __bytes__(self):
-		return (bytes([ord(self.typecode)])+bytes(array(self.typecode,self)))
+		return (bytes([ord(self.typecode)])+bytes(array(self.typecode,self)))  # 把typecode转换成字节序列，迭代Vector2d实例，得到一个数组，再把数组转换成字节序列。
 	def __eq__(self,other):
-		return tuple(self)==tuple(other)
+		return tuple(self)==tuple(other)  # 为了快速比较所有分量，在操作数中构建元祖
 	def __abs__(self):
-		return math.hypot(self.x,self.y)
+		return math.hypot(self.x,self.y)  # 模是x和y分量构成的直角三角形的斜边长。
 	def __bool__(self):
-		return bool(abs(self))
+		return bool(abs(self))  # 计算亩，把结果换成布尔值，非零值是True.
 	@classmethod
 	def frombytes(cls,octets):
 		typecode = chr(octets[0])
