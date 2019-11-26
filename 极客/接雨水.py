c47d60af3571312e079653b222f33bafe4954a28
xuysang <807692755@qq.com>
2019-11-26 22:33:52 +0800

    11-26-2

--- 极客/接雨水.py
+++ 极客/接雨水.py
@@ -1,14 +1,20 @@
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    def trap(self, height):
         sum = 0
         n = len(height)
         max_left = [0]*n
         max_right =[0]*n
-        min = 0
         
-        for i in range(1,n):
+        for i in range(1,n-1):
             max_left[i] = max(max_left[i-1],height[i-1])
         for i in range(n-2,-1,-1):
             max_right[i] = max(max_right[i+1],height[i+1])
-        for i in range(1,n):
-            sum = sum + (min(max_left[i],max_right[i]) - height[i])
+        for i in range(1,n-1):
+            min_0= min(max_left[i],max_right[i])
+            if (min_0 > height[i]):
+                sum = sum + (min_0 - height[i])
+        print(sum)
+        return sum
+a = Solution()
+height = [0,1,0,2,1,0,1,3,2,1,2,1]
+a.trap(height)
