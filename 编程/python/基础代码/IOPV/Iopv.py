aeab58f7645cb22b57492de1d7209442789decd3
xuysang <807692755@qq.com>
2019-12-11 16:00:32 +0800

    12-11-c

--- 编程/python/基础代码/IOPV/Iopv.py
+++ 编程/python/基础代码/IOPV/Iopv.py
@@ -1,12 +1,34 @@
+import requests
+import json
+import xlsxwriter
 
 
+def IOPV(code):
+    url = 'http://www.szse.cn/api/market/ssjjhq/getTimeData?random=0.3723049134781662&marketId=1&code=%s'%(code)
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    }
+    login_page = requests.get(url, headers=headers)
+    mydict = json.loads(login_page.text)
+    try:
+        iopv_value = mydict['data']['netValue']
+        return iopv_value
+    except KeyError:
+        pass
 
-url = http://www.szse.cn/api/market/ssjjhq/getTimeData?random=0.3723049134781662&marketId=1&code=159919
 
-
-
-
+code_list=[]
 with open('code.txt','r') as file:
     for line in file:
         code=line.replace('\n','')
         code_list.append(code)
+
+row = 0
+col = 0
+workbook = xlsxwriter.Workbook('iopv.xlsx')
+worksheet = workbook.add_worksheet()
+for eachone in code_list:
+    worksheet.write(row, col, eachone)
+    worksheet.write(row, col + 1, IOPV(eachone))
+    row += 1
+workbook.close()
